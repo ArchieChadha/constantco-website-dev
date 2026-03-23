9478285c19dd40271563a42886528d408768bca7
Ashraf172001 <ashrafulislam1817@gmail.com>
2026-03-23 12:51:32 +1100

    Updated file size

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+    limits: { fileSize: 100 * 1024 * 1024 }, // 100MB limit
     fileFilter: (req, file, cb) => {
         const allowedTypes = ['application/pdf', 'image/png', 'image/jpeg'];
 
